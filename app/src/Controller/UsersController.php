a768976785d281f008b36a182bddf8f3b32e169f
Dominic Boutin <dominic.boutin@equisoft.com>
2014-08-15 15:58:48 -0400

    Remove modules directory

--- app/src/Controller/UsersController.php
+++ app/src/Controller/UsersController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Users\Controller
+namespace Controller
 {
     use Form\Type\UserType;
     use Model\Entities\User;
@@ -8,13 +8,14 @@ namespace Users\Controller
 	use Silex\ControllerProviderInterface;
     use Symfony\Component\Form\FormError;
 
-    class AdminController implements ControllerProviderInterface
+    class UsersController implements ControllerProviderInterface
 	{
 		public function connect( Application $app )
 		{
 			$homeController = $app['controllers_factory'];
 			$homeController->get("/", array( $this, 'index' ) )->bind( 'admin-users' );
-            $homeController->match("/add-user", array( $this, 'new_user' ) )->bind( 'admin-add-user' );
+            $homeController->get("/user/{id}", array( $this, 'user' ) )->bind( 'admin-user' );
+            $homeController->match("/add-user", array( $this, 'add_user' ) )->bind( 'admin-add-user' );
 			
 			return $homeController;
 		}
@@ -27,7 +28,24 @@ namespace Users\Controller
 			return $app['twig']->render('users/index.html.twig', array('users' => $users));
 		}
 
-        public function new_user( Application $app )
+        public function user( Application $app, $id )
+        {
+            $repository = $app['em']->getRepository('\Model\Entities\User');
+            $users = $repository->findById($id);
+
+            if (count($users) > 0)
+            {
+                $user = $users[0];
+                return $app['twig']->render('users/user.html.twig', array('user' => $user));
+            }
+            else
+            {
+                $app['session']->getFlashBag()->add('warning', 'User do not exist');
+                return $app->redirect('/admin/users/');
+            }
+        }
+
+        public function add_user( Application $app )
         {
             $user = new User();
             $builder = $app['form.factory']->createBuilder(new UserType(), $user);
